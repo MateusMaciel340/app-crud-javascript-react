809117e6a4cbb2c97c85f84b5a085f69259577c3
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-16 10:25:33 -0300

    feat: component with login base structure

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,32 @@
+import "./style/Login.css";
+import LoginLogo from "../../assets/logo-login.png";
+
 const Login = () => {
     return (
-        <div>
-            <h1>Login!</h1>
+        <div className="text-center">
+            <main className="form-signin w-100 m-auto">
+                <form>
+                    <img className="mb-4"
+                        src={LoginLogo}
+                        alt="" width="200px" />
+                    <h1 className="h3 mb-3 fw-normal">Login</h1>
+
+                    <div className="form-floating">
+                        <input type="email" className="form-control"
+                            id="floatingInput" placeholder="name@example.com" />
+                        <label htmlFor="floatingInput">Digite seu email</label>
+                    </div>
+
+                    <div className="form-floating">
+                        <input type="password" className="form-control"
+                            id="floatingPassword" placeholder="Password" />
+                        <label htmlFor="floatingInput">Digite sua senha</label>
+                    </div>
+
+                    <button className="w-100 btn btn-lg btn-primary"
+                        type="submit">Logar</button>
+                </form>
+            </main>
         </div>
     );
 }
