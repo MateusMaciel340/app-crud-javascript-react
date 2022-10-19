a9e978213c5bb5971d40035350121f26ad5da87c
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-19 18:47:13 -0300

    feat: component to select users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,27 @@
+import "../CardHome/style/CardHome.css";
+
+import TableUser from "../TableUser/TableUser";
+
 const Users = () => {
     return (
-        <h1>Aqui fica as tabela de usuários</h1>
+        <div className="container style-card-general">
+            <div className="row">
+                <div className="col-md-8">
+                    <h1>Usuários</h1>
+                </div>
+                <div className="col-md-4 text-center">
+                    <button className="btn btn-primary">
+                        Adicionar novo usuário
+                    </button>
+                </div>
+
+                <hr/>
+
+                <div className="col-md-12">
+                    <TableUser/>
+                </div>
+            </div>
+        </div>
     );
 }
 
