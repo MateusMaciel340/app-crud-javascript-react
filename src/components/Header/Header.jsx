90af5d92385a0a8a1d9e13fddcc146c40f13d82b
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-21 16:26:30 -0300

    feat: logic to show top when logged in

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,67 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 import LogoHeader from "../../assets/crud.png";
+import { AuthenticatedUser } from "../../services/users/ApiUsers";
+
 
 const Header = () => {
+
+    const [userLogged, setUserLogged] = useState([]);
+
+    const getAuthenticatedUser = async () => {
+        const response = await AuthenticatedUser();
+
+        setUserLogged(response);
+    }
+
+    useEffect(() => {
+        getAuthenticatedUser()
+    }, []);
 
     const Logout = () => {
         window.location.reload(localStorage.clear());
     }
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-            <div className="container-fluid">
-                <img src={LogoHeader} width={90} height={65} />
-
-                <div className="collapse navbar-collapse"
-                    id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link
-                                className="nav-link active text-light"
-                                aria-current="page" to="/home">Home</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link
-                                className="nav-link active text-light"
-                                aria-current="page" to="/users">Users</Link>
-                        </li>
-                    </ul>
-
-                    <form className="d-flex" role="search">
-                        <Link to="/" onClick={Logout}>
-                            <button className="btn btn-danger"
-                                type="submit">
-                                <i className="fa fa-sign-out"></i>
-                            </button>
-                        </Link>
-                    </form>
+    if (userLogged === 401) {
+        return (
+            <></>
+        )
+    } else {
+        return (
+            <nav className="navbar navbar-expand-lg bg-dark">
+                <div className="container-fluid">
+                    <img src={LogoHeader} width={90} height={65} />
+    
+                    <div className="collapse navbar-collapse"
+                        id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link active text-light"
+                                    aria-current="page" to="/home">Home</Link>
+                            </li>
+    
+                            <li className="nav-item">
+                                <Link
+                                    className="nav-link active text-light"
+                                    aria-current="page" to="/users">Users</Link>
+                            </li>
+                        </ul>
+    
+                        <form className="d-flex" role="search">
+                            <Link to="/" onClick={Logout}>
+                                <button className="btn btn-danger"
+                                    type="submit">
+                                    <i className="fa fa-sign-out"></i>
+                                </button>
+                            </Link>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    );
+            </nav>
+        );
+    }
 }
 
 export default Header;
